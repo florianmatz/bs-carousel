5fc41caca8becd97caa6d27cfb1fa83ed78d8bdc
Florian Speckmaier <fspeckmaier@gmail.com>
2013-12-17 14:45:05 +0100

    Update carousel.js
    Fix to avoid removal of user set classes on carousel items.

--- source/assets/js/carousel.js
+++ source/assets/js/carousel.js
@@ -24,171 +24,199 @@
   // =========================
 
   var Carousel = function (element, options) {
-    this.$element    = $(element)
-    this.$indicators = this.$element.find('.carousel-indicators')
-    this.options     = options
+    this.$element    = $(element);
+    this.$indicators = this.$element.find('.carousel-indicators');
+    this.options     = options;
     this.paused      =
     this.sliding     =
     this.interval    =
     this.$active     =
-    this.$items      = null
+    this.$items      = null;
 
-    /*this.options.pause == 'hover' && this.$element
+    this.options.pause == 'hover' && this.$element
       .on('mouseenter', $.proxy(this.pause, this))
-      .on('mouseleave', $.proxy(this.cycle, this)) */
-
-    this.getActiveIndex();
-
-    this.init();
-
-  }
+      .on('mouseleave', $.proxy(this.cycle, this));
+  };
 
   Carousel.DEFAULTS = {
     interval: 5000,
     pause: 'hover',
     wrap: true,
-  }
+  };
 
-  Carousel.prototype.init = function() {
-    this.$items.not(this.$active).css('top', '100%');
-  }
 
   Carousel.prototype.cycle =  function (e) {
-    console.log('shall cycle');
-  }
+
+    e || (this.paused = false);
+
+    this.interval && clearInterval(this.interval);
+
+    this.options.interval
+      && !this.paused
+      && (this.interval = setInterval($.proxy(this.next, this), this.options.interval))
+
+    return this;
+
+  };
 
   Carousel.prototype.getActiveIndex = function () {
-    this.$active = this.$element.find('.item.active')
-    this.$items  = this.$active.parent().children().removeClass('no-transition');
+    this.$active = this.$element.find('.item.active');
+    this.$items  = this.$active.parent().children();
 
 
-    return this.$items.index(this.$active)
-  }
+    return this.$items.index(this.$active);
+  };
 
   Carousel.prototype.to = function (pos) {
 
-    var activeIndex = this.getActiveIndex()
+    var activeIndex = this.getActiveIndex(),
+        that = this;
 
-    // wenn quatsch aufgerufen wird
-    if (pos > (this.$items.length - 1) || pos < 0) return;
-
-    // wenn das gleiche item...
-    if( activeIndex === parseInt(pos) ) {
-      console.log('bin der gleiche');
+    if (pos > (this.$items.length - 1) || pos < 0) {
       return;
     }
 
-    // return ums chainable zu halten,
-    // wenn pos größer als der aktive index, dann nächste, also das item rechts davon, ansonsten das item links davon
-    return this.slide(pos > activeIndex ? 'next' : 'prev', $(this.$items[pos]))
-
-
-  }
-
-  Carousel.prototype.pause = function (e) {
-    console.log('shall pause');
-  }
-
-  Carousel.prototype.next = function () {
-    console.log('next');
-  }
-
-  Carousel.prototype.prev = function () {
-     console.log('prev');
-  }
-
-  Carousel.prototype.slide = function (type, nextItem) {
-
-    // aktiv nach links fahren
-    if(type == 'next') {
-
-      nextItem.css({
-        'top': '0',
-        'left': '100%'
-      });
-
-      nextItem.one('webkitTransitionEnd', $.proxy(this.activateItem,this));
-
-      // animate
-      nextItem.css({
-        '-webkit-transform': 'translate(-100%,0)'
-      });
-
-      this.$active.css({
-        '-webkit-transform': 'translate(-100%,0)',
-      }).removeClass('active');
-
-    }else if(type == 'prev') {
-
-      nextItem.css({
-        'top': '0',
-        'left': '-100%'
-      });
-
-      nextItem.one('webkitTransitionEnd', $.proxy(this.activateItem,this));
-
-      nextItem.css({
-        '-webkit-transform': 'translate(100%,0)'
-      });
-
-      this.$active.css({
-        '-webkit-transform': 'translate(100%,0)',
-      }).removeClass('active');
-
+    if (this.sliding) {
+      return this.$element.one('slid', function () { that.to(pos); });
     }
 
+    if( activeIndex === parseInt(pos,0) ) {
+      return this.pause().cycle();
+    }
 
-  }
-
-  Carousel.prototype.activateItem = function(evt) {
-
-    var $item = $(evt.target).addClass('active');
-
-    this.$active.add($item).addClass('no-transition'),
-
-    this.$active.css({
-      'left': '0',
-      'top': '100%',
-      '-webkit-transform': 'translate(0,0)'
-    });
-
-    $item.css({
-      'left': '0',
-      '-webkit-transform': 'translate(0,0)'
-    });
+    return this.slide(pos > activeIndex ? 'next' : 'prev', $(this.$items[pos]));
 
   };
+
+  Carousel.prototype.pause = function (e) {
+
+    e || (this.paused = true);
+
+    // hier noch animaton end abfragen, transition end tut zwar auch, aber zur sicherheit!
+    if (this.$element.find('.next, .prev').length && $.support.transition.end) {
+      this.$element.trigger($.support.transition.end);
+      this.cycle(true);
+    }
+
+    this.interval = clearInterval(this.interval);
+
+    return this;
+
+  };
+
+  Carousel.prototype.next = function () {
+    if (this.sliding) return;
+    return this.slide('next');
+  };
+
+  Carousel.prototype.prev = function () {
+    if (this.sliding) return;
+    return this.slide('prev');
+  };
+
+  Carousel.prototype.slide = function (type, next) {
+
+    var $active        = this.$element.find('.item.active'),
+        $next          = next || $active[type](),
+        isCycling      = this.interval,
+        animateActive  = type == 'next' ? 'center-to-left'  : 'center-to-right',
+        animateNext    = type == 'next' ? 'right-to-center' : 'left-to-center',
+        direction      = type == 'next' ? 'left'            :  'right',
+        fallback       = type == 'next' ?  'first'          : 'last',
+        that           = this;
+
+
+    if (!$next.length) {
+      if (!this.options.wrap) return;
+      $next = this.$element.find('.item')[fallback]();
+    }
+
+    this.sliding = true;
+
+    isCycling && this.pause();
+
+    var e = $.Event('slide.bs.carousel', { relatedTarget: $next[0], direction: direction });
+
+    if ($next.hasClass('active')) return;
+
+    if (this.$indicators.length) {
+      this.$indicators.find('.active').removeClass('active');
+      this.$element.one('slid', function () {
+        var $nextIndicator = $(that.$indicators.children()[that.getActiveIndex()]);
+        $nextIndicator && $nextIndicator.addClass('active'); // versteh ich auch noch nicht
+      });
+    }
+
+    // ggf $.support mit modernizr ersetzen, hier:
+    if ($.support.transition && this.$element.hasClass('slide')) {
+      this.$element.trigger(e);
+      if (e.isDefaultPrevented()) return;
+
+      $next.addClass(type);
+      $active.addClass(animateActive);
+      $next.addClass(animateNext);
+
+      // hier ggf Werte aus modernizr nehmen
+      $active.one('animationend webkitAnimationEnd oanimationend MSAnimationEnd', function() {
+        //$active.attr('class','item');
+        $active.add($next)
+            .removeClass('active next prev')
+            .removeClass(animateActive)
+            .removeClass(animateNext);
+
+        $next.addClass('active');
+        that.sliding = false;
+        that.$element.trigger('slid');
+      });
+
+
+    } else {
+      this.$element.trigger(e);
+      if (e.isDefaultPrevented()) return;
+      $active.removeClass('active');
+      $next.addClass('active');
+      this.sliding = false;
+      this.$element.trigger('slid');
+    }
+
+    isCycling && this.cycle();
+
+    return this;
+
+
+  };
+
 
 
   // CAROUSEL PLUGIN DEFINITION
   // ==========================
 
-  var old = $.fn.carousel
+  var old = $.fn.carousel;
 
   $.fn.carousel = function (option) {
     return this.each(function () {
-      var $this   = $(this)
-      var data    = $this.data('bs.carousel')
-      var options = $.extend({}, Carousel.DEFAULTS, $this.data(), typeof option == 'object' && option)
-      var action  = typeof option == 'string' ? option : options.slide
+      var $this   = $(this);
+      var data    = $this.data('bs.carousel');
+      var options = $.extend({}, Carousel.DEFAULTS, $this.data(), typeof option == 'object' && option);
+      var action  = typeof option == 'string' ? option : options.slide;
 
-      if (!data) $this.data('bs.carousel', (data = new Carousel(this, options)))
-      if (typeof option == 'number') data.to(option)
-      else if (action) data[action]()
-      else if (options.interval) data.pause().cycle()
-    })
-  }
+      if (!data) $this.data('bs.carousel', (data = new Carousel(this, options)));
+      if (typeof option == 'number') data.to(option);
+      else if (action) data[action]();
+      else if (options.interval) data.pause().cycle();
+    });
+  };
 
-  $.fn.carousel.Constructor = Carousel
+  $.fn.carousel.Constructor = Carousel;
 
 
   // CAROUSEL NO CONFLICT
   // ====================
 
   $.fn.carousel.noConflict = function () {
-    $.fn.carousel = old
-    return this
-  }
+    $.fn.carousel = old;
+    return this;
+  };
 
 
   // CAROUSEL DATA-API
@@ -196,25 +224,27 @@
 
   $(document).on('click.bs.carousel.data-api', '[data-slide], [data-slide-to]', function (e) {
 
-    var $this   = $(this), href
-    var $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) //strip for ie7
-    var options = $.extend({}, $target.data(), $this.data())
-    var slideIndex = $this.attr('data-slide-to')
-    if (slideIndex) options.interval = false
+    var $this   = $(this), href;
+    var $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')); //strip for ie7
+    var options = $.extend({}, $target.data(), $this.data());
+    var slideIndex = $this.attr('data-slide-to') || $this.attr('data-slide');
 
-
-    if (slideIndex = $this.attr('data-slide-to')) {
-      $target.data('bs.carousel').to(slideIndex)
+    // mein zeugs
+    if(slideIndex === 'next' || slideIndex === 'prev') {
+      $target.data('bs.carousel')[slideIndex]();
+    }else {
+      $target.data('bs.carousel').to(slideIndex);
     }
 
-    e.preventDefault()
-  })
+
+    e.preventDefault();
+  });
 
   $(window).on('load', function () {
     $('[data-ride="carousel"]').each(function () {
-      var $carousel = $(this)
-      $carousel.carousel($carousel.data())
-    })
-  })
+      var $carousel = $(this);
+      $carousel.carousel($carousel.data());
+    });
+  });
 
 }(jQuery);
